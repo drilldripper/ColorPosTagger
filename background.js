006d729f94d87c50b696097c13beffa227d53b53
drilldripper <ryo.kt.in@gmail.com>
2016-07-17 00:13:13 +0900

    Add on/off button icon

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-var message = true;
 chrome.browserAction.onClicked.addListener(function (id, bookmark) {
 	console.log("BACKGROUND");
 	// 取得するタブのクエリ
@@ -11,15 +10,30 @@ chrome.browserAction.onClicked.addListener(function (id, bookmark) {
 	chrome.tabs.query(queryInfo, function (result) {
 		// 配列の先頭に現在タブの情報が入っている
 		var currentTab = result.shift();
-		if(message == true){
+		if (message == true) {
 			message = false
-		}else{
+		} else {
 			message = true;
 		}
 		// メッセージ
-		// var message = true;
+		var message = {};
 
 		// 現在表示しているタブにメッセージを送る
 		chrome.tabs.sendMessage(currentTab.id, message, function () { });
 	});
+});
+
+
+//アイコンの更新
+var toggle = false;
+chrome.browserAction.onClicked.addListener(function(tab) {
+  toggle = !toggle;
+  if(toggle){
+    chrome.browserAction.setIcon({path: "icon1.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {file:"SCRIPT.user.js"});
+  }
+  else{
+    chrome.browserAction.setIcon({path: "icon2.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {code:"alert()"});
+  }
 });
